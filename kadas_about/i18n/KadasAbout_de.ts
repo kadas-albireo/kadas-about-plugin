dd78daa3a24b676ba1195956227fbae563501d1e
Sandro Mani <manisandro@gmail.com>
2021-06-24 12:19:03 +0200

    Update translations

--- kadas_about/i18n/KadasAbout_de.ts
+++ kadas_about/i18n/KadasAbout_de.ts
@@ -1,56 +1,62 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../about_dialog.py" line="38"/>
+        <location filename="../about_dialog.py" line="40"/>
         <source>This software is released under the &lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GNU Public License (GPL) Version 2&lt;/a&gt;</source>
         <translation>Diese Software steht unter der &lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GNU Public License (GPL) Version 2&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="71"/>
         <source>Terms of use for swisstopo geodata</source>
-        <translation>Nutzungsbedingungen Geodaten swisstopo</translation>
+        <translation type="obsolete">Nutzungsbedingungen Geodaten swisstopo</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="82"/>
         <source>Terms of use for Swiss GDI geodata</source>
-        <translation>Nutzungsbedingungen Geodaten Swiss GDI</translation>
+        <translation type="obsolete">Nutzungsbedingungen Geodaten Swiss GDI</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="96"/>
         <source>Data usage liability disclaimer</source>
-        <translation>Haftungsausschluss bei Datennutzung</translation>
+        <translation type="obsolete">Haftungsausschluss bei Datennutzung</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="25"/>
+        <location filename="../about_dialog.py" line="27"/>
         <source>About %s</source>
         <translation>Über %s</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="34"/>
         <source>&lt;b&gt;Version&lt;/b&gt;: %s (%s)</source>
-        <translation>&lt;b&gt;Version&lt;/b&gt;: %s (%s)</translation>
+        <translation type="obsolete">&lt;b&gt;Version&lt;/b&gt;: %s (%s)</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="43"/>
         <source>The MSS/MilX components are property of gs-soft AG</source>
-        <translation>Die MSS/MilX Komponenten sind Eigentum der gs-soft AG</translation>
+        <translation type="obsolete">Die MSS/MilX Komponenten sind Eigentum der gs-soft AG</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="87"/>
         <source>Data management and controller contact</source>
-        <translation>Kontakt Datenherr und Datenbereitstellung</translation>
+        <translation type="obsolete">Kontakt Datenherr und Datenbereitstellung</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="106"/>
         <source>&lt;b&gt;Swiss Armed Forces&lt;br /&gt;Mil Geo Info D&lt;br /&gt;VZ VBS&lt;br /&gt;3003 Bern&lt;/b&gt;</source>
-        <translation>&lt;b&gt;Swiss Armed Forces&lt;br /&gt;Mil Geo Info D&lt;br /&gt;VZ VBS&lt;br /&gt;3003 Bern&lt;/b&gt;</translation>
+        <translation type="obsolete">&lt;b&gt;Swiss Armed Forces&lt;br /&gt;Mil Geo Info D&lt;br /&gt;VZ VBS&lt;br /&gt;3003 Bern&lt;/b&gt;</translation>
     </message>
     <message>
         <location filename="../about_dialog.py" line="101"/>
         <source>BAC-Hotline: 058 465 22 22</source>
-        <translation>BAC-Hotline: 058 465 22 22</translation>
+        <translation type="obsolete">BAC-Hotline: 058 465 22 22</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="36"/>
+        <source>&lt;b&gt;Version&lt;/b&gt;: %s (%s) - Based on QGIS %s</source>
+        <translation>&lt;b&gt;Version&lt;/b&gt;: %s (%s) - Basierend auf QGIS %s</translation>
     </message>
 </context>
 </TS>
